3c635e99057e585954806ce50fa2b769c221871a
YashMistry1406 <yashmistry2000@gmail.com>
2024-05-07 16:30:53 +0530

    Changed the directory names

---
+++ Step 3/Lec 3 - Hard/CountInversion.java
@@ -0,0 +1,54 @@
+import java.util.*;
+
+/**
+ * CountInversion
+ */
+public class CountInversion {
+
+    public static int merge(int[] arr, int low, int mid, int high) {
+        ArrayList<Integer> temp = new ArrayList<>();
+
+        int left = low;
+        int right = mid + 1;
+        int cnt = 0;
+        while (left <= mid && right <= high) {
+            if (arr[left] <= arr[right]) {
+                temp.add(arr[left]);
+                left++;
+            } else {
+                temp.add(arr[right]);
+                cnt += (mid - left + 1);
+                right++;
+            }
+
+        }
+
+        while (left < mid) {
+            temp.add(arr[left]);
+            left++;
+        }
+        while (right <= high) {
+            temp.add(arr[right]);
+            right++;
+        }
+
+        for (int i = low; i <= high; i++) {
+            arr[i] = temp.get(i - low);
+        }
+
+        return cnt;
+
+    }
+
+    public static int MergeSort(int[] arr, int low, int high) {
+        int cnt = 0;
+        if (low >= high)
+            return cnt;
+        int mid = (low + high) / 2;
+        cnt+=MergeSort(arr, low, mid);
+        cnt+=MergeSort(arr, mid + 1, high);
+        cnt+=merge(arr, low, mid, high);
+
+        return cnt;
+    }
+}
